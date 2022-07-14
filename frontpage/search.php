bfd74c88db4d559cec8ea287ce25e4df6d3adcc9
chechi992 <107464637+chechi992@users.noreply.github.com>
2022-07-14 15:30:05 +0800

    加上展覽網站0714

--- frontpage/search.php
+++ frontpage/search.php
@@ -176,6 +176,14 @@ include 'header.php';
     // }
     if($queryResult > 0){
         while ($row = mysqli_fetch_assoc($result)){
+            // echo "<a href = '".$row['a_id'].".php'><div class='search-box'>
+            // <br>
+            // <h4>".$row['a_title']."</h4>
+            // <p>".$row['a_text']."</p>
+            // <p>".$row['a_date']."</p>
+            // <p>".$row['a_author']."</p>
+            // </div></a>";
+
             echo "<a href = '".$row['a_id'].".php'><div class='search-box'>
             <br>
             <h4>".$row['a_title']."</h4>
